2e718a833caf76c10f12a7a99ae26cd0cb081a4c
Jayarama Kapil Sridhara <46280713+jsrdhara@users.noreply.github.com>
2019-02-18 20:00:15 -0600

    Add files via upload

---
+++ Search.py
@@ -0,0 +1,15 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+def geturl(input_list):
+    query_result=[]
+    for i in input_list:
+        inner_list=[]
+        for j in search(i, tld="com", num=1, stop=1, pause=2): 
+            inner_list.append(j)
+        query_result.append(inner_list[0])
+    return query_result
+
